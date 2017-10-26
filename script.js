19b4faad2aa9eaa9892cbbf39fa55203d39e097b
ansellwill <willansell1@hotmail.com>
2017-10-27 09:31:21 +1100

    Add easy, hard and extra hard button functionality
    Some minor changes to make the code eaier to read however, lots of refactoring still needed

--- script.js
+++ script.js
@@ -24,27 +24,73 @@ var title = document.querySelector("h2");
 var resetButton = document.querySelector("#reset");
 var easybtn = document.querySelector("#easybtn");
 var hardbtn = document.querySelector("#hardbtn");
+var extraHardbtn = document.querySelector("#extraHardbtn");
+
+
+function startGame() {
+  arr = [];
+  generateNumberLine();
+  selectPokemon();
+  for(var i = 0; i < difficulty; i++){
+    // Use random numbers to add pokemon images
+  square[i].style.backgroundImage = "url(" + pokemon[arr[i]].image + ")";
+  //add click listeners to squares
+  square[i].addEventListener("click",function(){
+    //Get Name of clicked square
+  var clickedPokemon = this.style.backgroundImage;
+    // Compare Pokemon to clicked image
+  if (clickedPokemon === pickedPokemon) {
+    messageDisplay.textContent = "Correct";
+    resetButton.textContent = "Play Again?";
+    changePokemon();
+  } else {
+    this.style.backgroundImage = "none";
+    messageDisplay.textContent = "Try Again"
+  }
+  })
+}
+    }
 
 easybtn.addEventListener("click", function(){
   hardbtn.classList.remove("selected");
   easybtn.classList.add("selected");
+  extraHardbtn.classList.remove("selected");
+  difficulty = 3;
+  for(var i = 0; i < square.length; i++){
+  square[i].style.backgroundImage = "none";
+  square[i].style.backgroundSize = "100%";
 }
-)
+  startGame();
+})
 
 hardbtn.addEventListener("click", function(){
   easybtn.classList.remove("selected");
   hardbtn.classList.add("selected");
+  extraHardbtn.classList.remove("selected");
+  difficulty = 6;
+  for(var i = 0; i < square.length; i++){
+  square[i].style.backgroundSize = "100%";
+  startGame();
+}
+}
+)
+
+extraHardbtn.addEventListener("click", function(){
+  easybtn.classList.remove("selected");
+  hardbtn.classList.remove("selected");
+  extraHardbtn.classList.add("selected");
+  difficulty = 6;
+  for(var i = 0; i < difficulty; i++){
+    // Zoom in on Pokemon picutres
+  square[i].style.backgroundSize = "250%";
+
+  startGame();
+}
 }
 )
 
 resetButton.addEventListener("click", function(){
-  arr = [];
-  resetButton.textContent = "New Pokemon";
-  generateNumberLine();
-  selectPokemon();
-  for(var i = 0; i < square.length; i++){
-  square[i].style.backgroundImage = "url(" + pokemon[arr[i]].image + ")";
-    }
+  startGame();
   }
 )
 
@@ -60,7 +106,6 @@ while(arr.length < difficulty){
   }
 }
 
-generateNumberLine();
 
 function selectPokemon() {
   var randomNumber = Math.floor(Math.random() * arr.length);
@@ -68,35 +113,14 @@ function selectPokemon() {
   var pickedPokemonName = pokemon[arr[randomNumber]].name;
   title.textContent = pickedPokemonName;
 }
-selectPokemon();
 
- // Use random numbers to select pokemon images
-
-for(var i = 0; i < square.length; i++){
-  square[i].style.backgroundImage = "url(" + pokemon[arr[i]].image + ")";
-
-
-
-  //add click listeners to squares
-  square[i].addEventListener("click",function(){
-    //Get Name of clicked square
-  var clickedPokemon = this.style.backgroundImage;
-    // Compare Pokemon to clicked image
-  if (clickedPokemon === pickedPokemon) {
-    messageDisplay.textContent = "Correct";
-    resetButton.textContent = "Play Again?";
-    changePokemon();
-  } else {
-    this.style.backgroundImage = "none";
-    messageDisplay.textContent = "Try Again"
-  }
-  });
-}
 
 function changePokemon(){
   //Loop through all squares
-  for(var i = 0; i < square.length; i++){
+  for(var i = 0; i < difficulty; i++){
   //Change each to the given Pokemon
   square[i].style.backgroundImage = pickedPokemon;
   }
 }
+
+startGame();
